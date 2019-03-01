291110f10513c929f0f9ddd706025709d63b8c1d
Justin Philip Fulkerson <jfulkerson@Justins-MacBook-Pro.local>
2019-02-28 18:42:50 -0800

    Adding First File :

---
+++ TwoSum.py
@@ -0,0 +1,27 @@
+def twoSum(nums,target):
+    """ Returns indeicies of nums for a given target"""
+    indicies = []
+    h_map = {}
+
+    for counter,value in enumerate(nums):
+        print("{},{}".format(counter,value))
+
+
+
+def main():
+    """ Used as main driver of program, and used to test function"""
+    twoSum([2, 7, 11, 15],9)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
